d180a47dba88d98256c6f74b8673ce929cd6f0cb
hewei <hewei@itfsw.com>
2017-07-17 17:40:10 +0800

    LogicalDeletePlugin 测试用例

---
+++ src/test/java/com/itfsw/mybatis/generator/plugins/LogicalDeletePluginTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.itfsw.mybatis.generator.plugins;
+
+import com.itfsw.mybatis.generator.plugins.tools.DBHelper;
+import com.itfsw.mybatis.generator.plugins.tools.MyBatisGeneratorTool;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * ---------------------------------------------------------------------------
+ *
+ * ---------------------------------------------------------------------------
+ * @author: hewei
+ * @time:2017/7/7 16:59
+ * ---------------------------------------------------------------------------
+ */
+public class LogicalDeletePluginTest {
+    /**
+     * 初始化数据库
+     */
+    @BeforeClass
+    public static void init() throws SQLException, IOException, ClassNotFoundException {
+        DBHelper.createDB("scripts/LogicalDeletePlugin/init.sql");
+    }
+
+    /**
+     * 测试配置异常
+     */
+    @Test
+    public void testWarnings() throws IOException, XMLParserException, InvalidConfigurationException, InterruptedException, SQLException {
+        MyBatisGeneratorTool tool = MyBatisGeneratorTool.create("scripts/LogicalDeletePlugin/mybatis-generator-with-unsupport-type.xml");
+        tool.generate();
+
+        Assert.assertEquals(tool.getWarnings().get(0), "itfsw(逻辑删除插件):tb逻辑删除列(ts_2)的类型不在支持范围（请使用数字列，字符串列，布尔列）！");
+    }
+}
