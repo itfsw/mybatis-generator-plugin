8223231d8b4b025f8af787f6178df6ca27450c21
hewei <hewei@itfsw.com>
2017-07-28 14:33:31 +0800

    官方ConstructorBased配置BUG临时修正插件

--- src/main/java/com/itfsw/mybatis/generator/plugins/ConstructorBasedBugFixPlugin.java
+++ src/main/java/com/itfsw/mybatis/generator/plugins/ConstructorBasedBugFixPlugin.java
@@ -20,6 +20,7 @@ import com.itfsw.mybatis.generator.plugins.utils.BasePlugin;
 import com.itfsw.mybatis.generator.plugins.utils.FormatTools;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.Plugin;
 import org.mybatis.generator.api.dom.java.JavaVisibility;
 import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.Parameter;
@@ -46,57 +47,64 @@ public class ConstructorBasedBugFixPlugin extends BasePlugin {
     @Override
     public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
 
-        // 有种情况下ModelBaseRecordClass不会生成不包含BLOBs的构造方法，造成selectOneByExample返回结果找不到对应的构造方法异常
-        if (introspectedTable.hasBLOBColumns() && !introspectedTable.getRules().generateRecordWithBLOBsClass()) {
-            // 添加没有BLOBs的构造方法
-            Method method = new Method();
-            method.setVisibility(JavaVisibility.PUBLIC);
-            method.setConstructor(true);
-            method.setName(topLevelClass.getType().getShortName());
-            commentGenerator.addGeneralMethodComment(method, introspectedTable);
+        // 有种情况下ModelBaseRecordClass不会生成不包含BLOBs的构造方法
+        if (introspectedTable.isConstructorBased() && introspectedTable.hasBLOBColumns() && introspectedTable.getBLOBColumns().size() == 1) {
+            // 判断是否已经生成了对应构造函数
+            String version = Plugin.class.getPackage().getImplementationVersion();
+            if (version != null){
+                String[] strs = version.split("\\.");
+                if (strs.length == 3 && strs[0].equals("1") && strs[1].equals("3") && Integer.parseInt(strs[2]) < 6){
+                    // 添加没有BLOBs的构造方法
+                    Method method = new Method();
+                    method.setVisibility(JavaVisibility.PUBLIC);
+                    method.setConstructor(true);
+                    method.setName(topLevelClass.getType().getShortName());
+                    commentGenerator.addGeneralMethodComment(method, introspectedTable);
 
-            // 使用没有blobs的字段
-            List<IntrospectedColumn> constructorColumns = introspectedTable.getNonBLOBColumns();
+                    // 使用没有blobs的字段
+                    List<IntrospectedColumn> constructorColumns = introspectedTable.getNonBLOBColumns();
 
-            for (IntrospectedColumn introspectedColumn : constructorColumns) {
-                method.addParameter(new Parameter(introspectedColumn.getFullyQualifiedJavaType(), introspectedColumn.getJavaProperty()));
-                topLevelClass.addImportedType(introspectedColumn.getFullyQualifiedJavaType());
-            }
-
-            StringBuilder sb = new StringBuilder();
-            if (introspectedTable.getRules().generatePrimaryKeyClass()) {
-                boolean comma = false;
-                sb.append("super(");
-                for (IntrospectedColumn introspectedColumn : introspectedTable.getPrimaryKeyColumns()) {
-                    if (comma) {
-                        sb.append(", ");
-                    } else {
-                        comma = true;
+                    for (IntrospectedColumn introspectedColumn : constructorColumns) {
+                        method.addParameter(new Parameter(introspectedColumn.getFullyQualifiedJavaType(), introspectedColumn.getJavaProperty()));
+                        topLevelClass.addImportedType(introspectedColumn.getFullyQualifiedJavaType());
                     }
-                    sb.append(introspectedColumn.getJavaProperty());
+
+                    StringBuilder sb = new StringBuilder();
+                    if (introspectedTable.getRules().generatePrimaryKeyClass()) {
+                        boolean comma = false;
+                        sb.append("super(");
+                        for (IntrospectedColumn introspectedColumn : introspectedTable.getPrimaryKeyColumns()) {
+                            if (comma) {
+                                sb.append(", ");
+                            } else {
+                                comma = true;
+                            }
+                            sb.append(introspectedColumn.getJavaProperty());
+                        }
+                        sb.append(");");
+                        method.addBodyLine(sb.toString());
+                    }
+
+                    List<IntrospectedColumn> introspectedColumns;
+                    if (!introspectedTable.getRules().generatePrimaryKeyClass() && introspectedTable.hasPrimaryKeyColumns()) {
+                        introspectedColumns = introspectedTable.getNonBLOBColumns();
+                    } else {
+                        introspectedColumns = introspectedTable.getBaseColumns();
+                    }
+
+                    for (IntrospectedColumn introspectedColumn : introspectedColumns) {
+                        sb.setLength(0);
+                        sb.append("this.");
+                        sb.append(introspectedColumn.getJavaProperty());
+                        sb.append(" = ");
+                        sb.append(introspectedColumn.getJavaProperty());
+                        sb.append(';');
+                        method.addBodyLine(sb.toString());
+                    }
+
+                    FormatTools.addMethodWithBestPosition(topLevelClass, method);
                 }
-                sb.append(");");
-                method.addBodyLine(sb.toString());
             }
-
-            List<IntrospectedColumn> introspectedColumns;
-            if (!introspectedTable.getRules().generatePrimaryKeyClass() && introspectedTable.hasPrimaryKeyColumns()) {
-                introspectedColumns = introspectedTable.getNonBLOBColumns();
-            } else {
-                introspectedColumns = introspectedTable.getBaseColumns();
-            }
-
-            for (IntrospectedColumn introspectedColumn : introspectedColumns) {
-                sb.setLength(0);
-                sb.append("this.");
-                sb.append(introspectedColumn.getJavaProperty());
-                sb.append(" = ");
-                sb.append(introspectedColumn.getJavaProperty());
-                sb.append(';');
-                method.addBodyLine(sb.toString());
-            }
-
-            FormatTools.addMethodWithBestPosition(topLevelClass, method);
         }
 
         return true;
