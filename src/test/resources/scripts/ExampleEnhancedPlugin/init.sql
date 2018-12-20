236b388545a8b8c52763b5435e9d3d7031e2b88d
hewei <hewei@itfsw.com>
2018-12-20 13:47:27 +0800

    增加测试用例

--- src/test/resources/scripts/ExampleEnhancedPlugin/init.sql
+++ src/test/resources/scripts/ExampleEnhancedPlugin/init.sql
@@ -39,3 +39,23 @@ INSERT INTO `tb` VALUES ('7', 'f7', '100');
 INSERT INTO `tb` VALUES ('8', 'f8', '100');
 INSERT INTO `tb` VALUES ('9', 'f9', '100');
 INSERT INTO `tb` VALUES ('10', 'f10', '100');
+
+-- ----------------------------
+-- Table structure for tb_all
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_all`;
+CREATE TABLE `tb_all` (
+  `key1` bigint(20) NOT NULL COMMENT '注释1',
+  `key2` varchar(20) NOT NULL COMMENT '注释2',
+  `field1` varchar(255) DEFAULT NULL COMMENT '注释3',
+  `field2` bigint(20) NOT NULL COMMENT '注释4',
+  `field3` longtext COMMENT '注释5',
+  PRIMARY KEY (`key1`, `key2`)
+);
+
+-- ----------------------------
+-- Records of tb_all
+-- ----------------------------
+INSERT INTO `tb_all` VALUES ('1', 'key1', 'f1', '1', 'b1');
+INSERT INTO `tb_all` VALUES ('2', 'key2', 'f2', '1', 'b2');
+INSERT INTO `tb_all` VALUES ('3', 'key3', 'f3', '1', 'b3');
